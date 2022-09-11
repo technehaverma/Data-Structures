acb19a68622c27558bcf6c2680173bd5ec9c6af5
Neha Verma <107301850+technehaverma@users.noreply.github.com>
2022-09-12 01:20:16 +0530

    new Question solution added

---
+++ src/com/practice/problems/JumpGameProblem.java
@@ -0,0 +1,49 @@
+package com.practice.problems;
+
+/*
+ * You have an array of non-negative integers,you are initially positioned at the 
+ * first index of the array.Each element in the array represents your maximum jump length at that position.
+ */
+public class JumpGameProblem {
+	private static int Jump(int[] num) {
+		int jump = 0;
+		int arrayTrav = num[0];
+		int maxTrav = num[0];
+		int i = 0;
+		while(i<num.length) {
+			
+			if(i>=num.length-1){
+				return jump;
+			}
+			--arrayTrav;
+			--maxTrav;
+			if(maxTrav<num[i]) {
+				maxTrav=num[i];
+			}
+			if(arrayTrav==0 && maxTrav==0) {
+				return -1;
+			}
+			if(arrayTrav==0) {
+				jump++;
+				arrayTrav=maxTrav;
+			}
+			i++;
+		}
+		return jump;
+	}
+
+	public static void main(String[] args) {
+		int num[] = {3,2,1,0,4};
+
+		System.out.println("Minimum number of jumps to reach end is : " + Jump(num));
+	}
+}
+
+/*
+ * Try more Inputs case 1: actual = Jump([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9])
+ * expected = 3
+ * 
+ * case2: actual = Jump([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) expected = 10
+ * 
+ * case3: actual = Jump([3,2,1,0,4]) expected = -1 Jump not possible
+ */
---
+++ src/com/practice/problems/Sort0toEndofList.java
@@ -0,0 +1,61 @@
+package com.practice.problems;
+
+public class Sort0toEndofList {
+	static void MoveZeroToEnd(int array[], int n) {
+		int i = 0;
+		int k = n;
+		while (i <= k) {
+			if (array[i] == 0) {
+				swap(array, i, k);
+				k--;
+			} else {
+				i++;
+			}
+		}
+
+	}
+
+	static void MoveZeroToEndMaintainingSequence(int array[], int n) {
+		int i = 0;
+		int j = 0;
+		while (j <= n) {
+			if (array[j] != 0) {
+				swap(array, i, j);
+				j++;
+				i++;
+			} else {
+
+				j++;
+			}
+		}
+
+	}
+
+	private static void swap(int[] array, int j, int k) {
+		int temp = array[j];
+		array[j] = array[k];
+		array[k] = temp;
+	}
+
+	public static void main(String[] args) {
+		int array[] = { 0, 1, 0, 3, 12 };
+		int n = array.length;
+
+		MoveZeroToEndMaintainingSequence(array, n - 1);
+
+		System.out.println("result is: ");
+		for (int i = 0; i < n; i++)
+			System.out.print(array[i] + " ");
+	}
+}
+
+/*
+ * case1: actual = MoveZeroToEndMaintainingSequence([1,3,0,0,4,0,9],7) expected =
+ * [1,3,4,9,0,0,0]
+ * 
+ * case2: actual = MoveZeroToEndMaintainingSequence([0,1,0,3,12],5) expected = [1,3,12,0,0]
+ * 
+ * case3: actual = MoveZeroToEndMaintainingSequence([0, 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9],13)
+ * expected = [1,9,8,4,2,7,6,9,0,0,0,0,0]
+ * 
+ */
